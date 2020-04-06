166a3ebeb2ffd01a6b80024ba4d021d66a8bb129
johnray-martinez <jrvmartinez@icloud.com>
2020-04-06 18:08:24 -0400

    added firebase to project

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './components/App/App';
 import * as serviceWorker from './serviceWorker';
 import * as firebase from 'firebase';
 
-let config = {
+const firebaseConfig = {
   apiKey: "AIzaSyD5dNdKlt4dn6Nb634yBlWtPdcWnEPwznM",
   authDomain: "chat-app-22717.firebaseapp.com",
   databaseURL: "https://chat-app-22717.firebaseio.com",
@@ -15,7 +15,7 @@ let config = {
   appId: "1:951478077758:web:f48ce20c649f63c80b846f",
   measurementId: "G-PXQNQW06TJ"
 };
-firebase.initializeApp(config);
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
