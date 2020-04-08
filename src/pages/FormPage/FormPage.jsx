723a624613fc3e34a5ce88c99172ca414b54fc23
Darkthunder119 <gurtajchhabra@gmail.com>
2020-04-07 20:12:23 -0400

    changes before the merge from John

--- src/pages/FormPage/FormPage.jsx
+++ src/pages/FormPage/FormPage.jsx
@@ -5,11 +5,13 @@ import * as firebase from 'firebase';
 export default function FormPage(props){
   const [activeTab, setActiveTab] = useState('login');
   const auth = firebase.auth();
-
+  const gitProvider = new firebase.auth.GithubAuthProvider();
+  const fbProvider = new firebase.auth.FacebookAuthProvider();
   document.title = `${activeTab === 'login' ? 'LOGIN | ' : 'SIGNUP | ' } Chat App`;
   if (sessionStorage.getItem('email')) props.history.push('/');
   // const db = firebase.firestore();
-
+  fbProvider.addScope('email');
+  gitProvider.addScope('user');
   const onSubmitHandler = e => {
     e.preventDefault();
     const {email, password, confirm} = e.target;
@@ -37,6 +39,11 @@ export default function FormPage(props){
     } else console.log('Invalid input');
   }
   const onClickHandler = e => setActiveTab(e.target.dataset.value);
+
+  const onGitHubHandler = e => auth.signInWithPopup(gitProvider).then(data=>console.log('token', data.credential.accessToken , 'user', data)).catch(err=>console.log(err));
+
+  const onFaceBookHandler = e => auth.signInWithPopup(fbProvider).then(data=>console.log('token', data.credential.accessToken, 'user', data)).catch(err=>console.log(err));
+  
   return(
     <div className="form-page">
       <div className="form-page__overlay"></div>
@@ -68,10 +75,13 @@ export default function FormPage(props){
           <div className="form-page__row form-page__row--button">
             <button className="form-page__button">SIGN {activeTab==='login'? `IN` : 'UP'}</button>
           </div>
-          <div className="form-page__row">
-            <button className="form-page__button form-page__button--fb"> {activeTab==='login'? `Connect` : 'Sign up'} with facebook</button>
-          </div>
         </form>
+        {activeTab === 'login' ? (<div className="form-page__row">
+            <button onClick={onGitHubHandler} className="form-page__button form-page__button--gh" name="git">Connect with Github</button>
+          </div>) : <></>}
+        {activeTab === 'login' ? (<div className="form-page__row">
+            <button onClick={onFaceBookHandler} className="form-page__button form-page__button--fb" name="face">Connect with Facebook</button>
+          </div>) : <></>}
       </div>
     </div>
   );
