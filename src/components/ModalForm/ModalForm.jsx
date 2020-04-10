04ef06069ad759abbe6a3c9cd642adaf4a4deee0
johnray-martinez <jrvmartinez@icloud.com>
2020-04-10 09:17:45 -0400

    fixed some styling for the modal form

--- src/components/ModalForm/ModalForm.jsx
+++ src/components/ModalForm/ModalForm.jsx
@@ -4,7 +4,7 @@ import './ModalForm.scss';
 export default function ModalForm({handler}){
   return(
     <form className="modal-form" onSubmit={handler}>
-      <h2>New Channel</h2>
+      <h2 className="modal-form__header">New Channel</h2>
       <label className="modal-form__label" htmlFor="name">Name</label> <br/>
       <input className="modal-form__input" type="text" name="name" maxLength="20" /> <br/>
       <label className="modal-form__label" htmlFor="password">Password (Optional)</label> <br/>
