6bb9a56dd3be4e14e276e4017ad99fa94343b62e
Darkthunder119 <gurtajchhabra@gmail.com>
2020-04-14 13:08:05 -0400

    refactored the 3 changehandlers into 1

--- src/components/ModalProfile/ModalProfile.jsx
+++ src/components/ModalProfile/ModalProfile.jsx
@@ -7,22 +7,25 @@ import 'firebase/firebase-database';
 
 export default function ModalProfile({hideModal}){
     const user = firebase.auth().currentUser;
-    const [phone, setPhone] = useState(user.phoneNumber);
-    const [name, setName] = useState(user.displayName);
-    const [image, setImage] = useState(user.photoURL);
+    const [value, setValue] = useState({
+        displayName: user.displayName,
+        photoURL : user.photoURL
+    });
+    const [phone, setPhone] = useState();
     const updateProfile = (e) =>{
         e.preventDefault();
         user.updateProfile({
-            displayName: name,
-            photoURL: image
+            displayName: value.displayName,
+            photoURL: value.photoURL
           }).then(res=>console.log('success')).catch(err=>console.log(err));
-        console.log(phone, name, image);
+        console.log(value, phone);
+        console.log(user);
         e.target.reset();
         hideModal();
     }
 
-    const handleChangeName = (e) => setName(e.target.value);
-    const handleChangeImage = (e) => setImage(e.target.value);
+    const handleChange = (e) => setValue({...value, [e.target.name] : e.target.value});
+    
 
     
   return(
@@ -31,15 +34,15 @@ export default function ModalProfile({hideModal}){
       <form onSubmit={updateProfile}>
         <div className="form-page__row">
             <label className="form-page__label" htmlFor="text">Display Name</label>
-            <input className="form-page__input" type="text" name="text" placeholder="Profile Display Name" value={name ? name : ''} onChange={handleChangeName} />
+            <input className="form-page__input" type="text" name="displayName" placeholder="Profile Display Name" value={value.displayName ? value.displayName : ''} onChange={handleChange} />
         </div>
         <div className="form-page__row">
             <label className="form-page__label" htmlFor="text-image">Display Picture</label>
-            <input className="form-page__input" type="url" name="textImage" placeholder="Enter Image URL Only" value={image ? image : ''} onChange={handleChangeImage}/>
+            <input className="form-page__input" type="url" name="photoURL" placeholder="Enter Image URL Only" value={value.photoURL ? value.photoURL : ''} onChange={handleChange}/>
         </div>
         <div className="form-page__row">
             <label className="form-page__label" htmlFor="phone">Phone Number</label>
-            <PhoneInput defaultCountry="CA" value={phone ? phone : ''} onChange={setPhone} className="modal-profile__phone" name="phone"/>
+            <PhoneInput defaultCountry="CA" value={phone ? phone : ''} onChange={setPhone} className="modal-profile__phone" name="phoneNumber"/>
         </div>
         <div className="form-page__row form-page__row--button">
             <button className="form-page__button">Update</button>
