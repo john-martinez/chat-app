267d5f20ca7e48e24b26725ef5174baabb3fa2b7
johnray-martinez <jrvmartinez@icloud.com>
2020-04-14 21:32:14 -0400

    added auto resizing message box

--- src/components/Chatroom/Chatroom.jsx
+++ src/components/Chatroom/Chatroom.jsx
@@ -9,6 +9,7 @@ import './Chatroom.scss';
 const Chatroom = React.forwardRef(({ user, channel, displayChannels},ref) => {
   const messagesContainer = useRef();
   const prevChannel = useRef();
+  const messageForm = useRef();
   const [msgs, setMsgs] = useState([]);
   const [text, setText] = useState('');
   const messages = firebase.database().ref('channels/' + channel[0]).child('messages');
@@ -38,19 +39,26 @@ const Chatroom = React.forwardRef(({ user, channel, displayChannels},ref) => {
     if (lastMsg) lastMsg.scrollIntoView();
   })
 
-  const sendMessage = e=>{
-    e.preventDefault();
-    const {message} = e.target;
+  const sendMessage = messageForm =>{
+    let { message } = messageForm.current;
     let messageObj = {
       message: message.value, 
       sender: user,
       timestamp: Date.now()
     }
     messages.push(messageObj);
-    e.target.reset();
+    setText(''); // clears the input field 
   }
 
-  const onChangeHandler = e => setText(e.target.value)
+  const onChangeHandler = e => { 
+    setText(e.target.value);
+  }
+  
+  const onKeypressHandler = e => {
+    if (e.key === 'Enter' && !e.shiftKey) {
+      sendMessage(messageForm);
+    }
+  }
   return(
     <div className="chat-room" ref={ref}>
       <Navbar channel={channel} displayChannels={displayChannels} />
@@ -64,11 +72,18 @@ const Chatroom = React.forwardRef(({ user, channel, displayChannels},ref) => {
           ))
         }
       </div>
-      <form onSubmit={sendMessage} className="chat-room__input-container">
+      <form onSubmit={sendMessage} className="chat-room__input-container" ref={messageForm}>
         <div className="chat-room__field-container">
           <label htmlFor="message"></label>
           <div className="chat-room__field-target">{text}</div>
-          <textarea className="chat-room__field" value={text} onChange={onChangeHandler} type="text" name="message" placeholder={`Message #${channel[1].name}`} ></textarea>
+          <textarea 
+            className="chat-room__field" 
+            value={text} 
+            onChange={onChangeHandler} 
+            onKeyPress={onKeypressHandler}
+            type="text" 
+            name="message" 
+            placeholder={`Message #${channel[1].name}`} ></textarea>
         </div>
         <div className="chat-room__field-icon"><FontAwesomeIcon icon={faCompressAlt} /></div>
       </form>
